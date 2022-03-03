baaa1944f69e6224a320f38eb552bf4d7ea58697
Andre De Carolis <andre.decarolis@gmail.com>
2022-03-03 17:42:34 +0000

    Separaçao de estilo, script e html

---
+++ script.js
@@ -0,0 +1,19 @@
+function calcular(tipo, valor) {
+        if (tipo === 'acao') {
+          if (valor === 'c') {
+            //limpar o visor (id resultado)
+            document.getElementById('resultado').value = ''
+          }
+          if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+            document.getElementById('resultado').value += valor
+          }
+          if (valor === '=') {
+            var valor_campo = eval(document.getElementById('resultado').value) //eval() pega uma string e transforma numa operaçao matematica
+            document.getElementById('resultado').value = valor_campo
+          }
+
+        } else if (tipo === 'valor') {
+          document.getElementById('resultado').value += valor
+        }
+
+      }
